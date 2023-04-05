4b770f76e10a2c20a278901a0d7a37e61ff42b99
PichaiThongarsa <119285554+PichaiThongarsa@users.noreply.github.com>
2023-04-06 03:49:08 +0700

    first commit

---
+++ Project Restaurant/Order.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace Project_Restaurant
+{
+    partial class Order
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.addFoodToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveFlieToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.orderFoodToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.orderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fastfoodToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.thaifoodToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button2 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Visible;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addFoodToolStripMenuItem,
+            this.orderFoodToolStripMenuItem,
+            this.menuToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(5, 3, 0, 3);
+            this.menuStrip1.Size = new System.Drawing.Size(1257, 25);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // addFoodToolStripMenuItem
+            // 
+            this.addFoodToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveFlieToolStripMenuItem});
+            this.addFoodToolStripMenuItem.Name = "addFoodToolStripMenuItem";
+            this.addFoodToolStripMenuItem.Size = new System.Drawing.Size(37, 19);
+            this.addFoodToolStripMenuItem.Text = "File";
+            // 
+            // saveFlieToolStripMenuItem
+            // 
+            this.saveFlieToolStripMenuItem.Name = "saveFlieToolStripMenuItem";
+            this.saveFlieToolStripMenuItem.Size = new System.Drawing.Size(117, 22);
+            this.saveFlieToolStripMenuItem.Text = "Save file";
+            // 
+            // orderFoodToolStripMenuItem
+            // 
+            this.orderFoodToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.orderToolStripMenuItem});
+            this.orderFoodToolStripMenuItem.Name = "orderFoodToolStripMenuItem";
+            this.orderFoodToolStripMenuItem.Size = new System.Drawing.Size(77, 19);
+            this.orderFoodToolStripMenuItem.Text = "Order food";
+            // 
+            // orderToolStripMenuItem
+            // 
+            this.orderToolStripMenuItem.Name = "orderToolStripMenuItem";
+            this.orderToolStripMenuItem.Size = new System.Drawing.Size(104, 22);
+            this.orderToolStripMenuItem.Text = "Order";
+            this.orderToolStripMenuItem.Click += new System.EventHandler(this.orderToolStripMenuItem_Click);
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fastfoodToolStripMenuItem,
+            this.thaifoodToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(50, 19);
+            this.menuToolStripMenuItem.Text = "Menu";
+            this.menuToolStripMenuItem.Click += new System.EventHandler(this.menuToolStripMenuItem_Click);
+            // 
+            // fastfoodToolStripMenuItem
+            // 
+            this.fastfoodToolStripMenuItem.Name = "fastfoodToolStripMenuItem";
+            this.fastfoodToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.fastfoodToolStripMenuItem.Text = "Fast food";
+            this.fastfoodToolStripMenuItem.Click += new System.EventHandler(this.fastfoodToolStripMenuItem_Click);
+            // 
+            // thaifoodToolStripMenuItem
+            // 
+            this.thaifoodToolStripMenuItem.Name = "thaifoodToolStripMenuItem";
+            this.thaifoodToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.thaifoodToolStripMenuItem.Text = "Thai food";
+            this.thaifoodToolStripMenuItem.Click += new System.EventHandler(this.thaifoodToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(0, 40);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(272, 50);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "ประวัติการสั่งซื้อ";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.Tan;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1});
+            this.dataGridView1.Location = new System.Drawing.Point(-13, 118);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(5);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(1270, 478);
+            this.dataGridView1.TabIndex = 9;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Column1
+            // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column1.HeaderText = "ประวัติการสั่งซื้อ";
+            this.Column1.Name = "Column1";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(473, 40);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(285, 50);
+            this.button2.TabIndex = 60;
+            this.button2.Text = "เมนู";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Order
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1257, 749);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Order";
+            this.Text = "Foodprice";
+            this.Load += new System.EventHandler(this.Order_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem addFoodToolStripMenuItem;
+        private ToolStripMenuItem saveFlieToolStripMenuItem;
+        private ToolStripMenuItem orderFoodToolStripMenuItem;
+        private ToolStripMenuItem orderToolStripMenuItem;
+        private ToolStripMenuItem menuToolStripMenuItem;
+        private ToolStripMenuItem fastfoodToolStripMenuItem;
+        private ToolStripMenuItem thaifoodToolStripMenuItem;
+        private Label label1;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn Column1;
+        private Button button2;
+    }
+}
---
+++ Project Restaurant/Order.cs
@@ -0,0 +1,75 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_Restaurant
+{
+    public partial class Order : Form
+    {
+        public Order()
+        {
+            InitializeComponent();
+            ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.Commercial;
+            using (ExcelPackage package = new ExcelPackage(new FileInfo(@"C:\Users\poste\Desktop\ปปxx\Project Restaurant\Project Restaurant\bin\Debug\output.xlsx")))
+            {
+                ExcelWorksheet worksheet = package.Workbook.Worksheets["Sheet1"];
+                int lastUsedRow = worksheet.Dimension.End.Row;
+                for (int row = 1; row <= lastUsedRow; row++)
+                {
+                    string value = worksheet.Cells[row, 1].Value?.ToString();
+                    if (!string.IsNullOrEmpty(value))
+                    {
+                        this.dataGridView1.Rows.Add(value);
+                    }
+                }
+
+            }
+        }
+
+        private void menuToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Order_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void orderToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Order order = new Order();
+            order.ShowDialog();
+        }
+
+        private void fastfoodToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Menufood menufood = new Menufood();
+            menufood.ShowDialog();
+        }
+
+        private void thaifoodToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Menufood menufood = new Menufood();
+            menufood.ShowDialog();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Menufood menufood = new Menufood();
+            menufood.ShowDialog();
+        }
+    }
+}
